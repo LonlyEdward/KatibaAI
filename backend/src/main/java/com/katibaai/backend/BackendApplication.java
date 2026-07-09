063c1a80b483ba8ca25932bb2f916e19b24e5ed7
LonlyEdward <lioneryedward1@gmail.com>
2026-07-10 00:15:39 +0300

    full docker compose setup

--- backend/src/main/java/com/katibaai/backend/BackendApplication.java
+++ backend/src/main/java/com/katibaai/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.katibaai.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
